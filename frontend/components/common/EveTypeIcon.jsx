e44b2024ed999d66127b827edc1c35d1ddbecf31
Dan Garfield <dan.garfield@tui.co.uk>
2023-11-08 10:02:48 +0000

    Ensure reaction formula icons work

--- frontend/components/common/EveTypeIcon.jsx
+++ frontend/components/common/EveTypeIcon.jsx
@@ -1,6 +1,6 @@
 const EveTypeIcon = (props) => {
   return (
-    <img src={`https://images.evetech.net/types/${props.type.type_id}/${props.type.name.includes('Blueprint') ? 'bp' : 'icon'}`} class='width-32' />
+    <img src={`https://images.evetech.net/types/${props.type.type_id}/${props.type.name.includes('Blueprint') || props.type.name.includes('Reaction Formula') ? 'bp' : 'icon'}`} class='width-32' />
   )
 }
 export default EveTypeIcon
